46635e8cfb23a4f194228a33d70951288cf330b5
AlfieriChou <alfierichou@gmail.com>
2018-10-27 10:23:10 +0800

    release v0.0.3 Using for loop

--- changelog.md
+++ changelog.md
@@ -1,5 +1,8 @@
 ### Oct 27, 2018
 
+  * release v0.0.3
+    * Using for loop to solve async, suport all node version
+
   * release v0.0.2
   	* convert dir path proto to jsonschema mdoels
 
--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const path = require('path')
 /**
  * @param {string} dirpath
  */
-async function convert (dirpath) {
+function convert (dirpath) {
   if (!dirpath) throw new Error('Dirpath required!!!')
   let component = {}
   const filenames = dir(dirpath)
-  await Promise.all(
-    filenames.map(async (filename) => {
-      let filenameArray = filename.split('/')
-      let popFileNameArr = filenameArray.pop()
-      let filePath = path.join(dirpath + '/' + popFileNameArr)
-      let modelName = popFileNameArr.replace(/\.\w+$/, '')
-      let schemaName = modelName.slice(0, 1).toUpperCase() + modelName.slice(1)
-      let schema = await compile(filePath, schemaName)
-      let type = schema.definitions[schemaName].type
-      let properties = schema.definitions[schemaName].properties
-      component[schemaName] = {
-        type: type,
-        properties: properties
-      }
-    })
-  )
+  for (let i = 0; i < filenames.length; ++i) {
+    let filename = filenames[i]
+    let filenameArray = filename.split('/')
+    let popFileNameArr = filenameArray.pop()
+    let filePath = path.join(dirpath + '/' + popFileNameArr)
+    let modelName = popFileNameArr.replace(/\.\w+$/, '')
+    let schemaName = modelName.slice(0, 1).toUpperCase() + modelName.slice(1)
+    let schema = compile(filePath, schemaName)
+    let schemaRealName = schema.definitions[schemaName].title
+    let type = schema.definitions[schemaName].type
+    let properties = schema.definitions[schemaName].properties
+    component[schemaRealName] = {
+      type: type,
+      properties: properties
+    }
+  }
   return component
 }
 
